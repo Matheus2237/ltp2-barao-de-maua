a37a64aea6ca69815f1e386a14eb75e72d84c92d
Matheus Paulino Ribeiro <mathpaulinor@gmail.com>
2023-02-12 15:49:35 -0300

    Inclusão de projeto feito em aula do dia 07-02-2023:
    - Primeiro hello world
    @author: Matheus Paulino <mathpaulinor@gmail.com>

---
+++ Aulas/Aula 00 - 07_02_2023/driver.cpp
@@ -0,0 +1,13 @@
+/*
+    Laboratório de Técnicas de Programação II - Aula 07/02/2023
+    Primeiro hello world
+    @author: Matheus Paulino <mathpaulinor@gmail.com>
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Ola mundo!";
+}
